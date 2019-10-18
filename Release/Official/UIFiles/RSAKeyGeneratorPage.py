132a18e57a307050cc46c71e33618b10bdc3ac3c
mgsedabat <mgseda@gmail.com>
2019-10-17 22:41:08 -0400

    Added functionality to RSA Key Generation public key and private key browse buttons

--- Release/Official/UIFiles/RSAKeyGeneratorPage.py
+++ Release/Official/UIFiles/RSAKeyGeneratorPage.py
@@ -11,6 +11,7 @@ class RSAKeyGeneratorPage(QtWidgets.QDialog):
     def __init__(self):
         super(RSAKeyGeneratorPage, self).__init__()
         uic.loadUi('./UIFiles/generate_rsa_keys.ui', self)
+        self.file_name = None
         self.public_key_file_path_button = self.findChild(QtWidgets.QPushButton, 'publicKeyFilePath')
         self.private_key_file_path_button = self.findChild(QtWidgets.QPushButton, 'privateKeyFilePath')
         self.public_key_line_edit = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
@@ -19,12 +20,27 @@ class RSAKeyGeneratorPage(QtWidgets.QDialog):
         self.generate_push_button = self.findChild(QtWidgets.QPushButton, 'generatePushButton')
         self.public_key_line_edit.setReadOnly(True)
         self.private_key_line_edit.setReadOnly(True)
-        self.private_key_file_path_button.clicked.connect(self.save_file)
+        self.public_key_file_path_button.clicked.connect(self.save_public)
+        self.private_key_file_path_button.clicked.connect(self.save_private)
 
-    def save_file(self):
-        file_path = FileService.saveFileDialog(self.Form, self.Form)
+    def save_public(self):
+        file_path = FileService.saveFileDialog(self, self)
+        self.file_name = file_path
         if file_path is not None and file_path != "":
-            file = FileService.write_file_content(self, file_path)
+            file = open(file_path, 'wb')
+            file.close()
+            self.public_key_line_edit.setText(file_path)
+        else:
+            #handle no file warning
+            return None
+
+    def save_private(self):
+        file_path = FileService.saveFileDialog(self, self)
+        self.file_name = file_path
+        if file_path is not None and file_path != "":
+            file = open(file_path, 'wb')
+            file.close()
+            self.private_key_line_edit.setText(file_path)
         else:
             #handle no file warning
             return None
